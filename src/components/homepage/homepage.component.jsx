3b85db537f14f9f28a2e2ca084b5efcd18391b1b
Raj <raj.21810934@viit.ac.in>
2021-06-03 19:50:39 +0530

    First commit

---
+++ src/components/homepage/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="heading">Hats </h1>
+            <span className="subtitle">Shop Here </span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="heading">Sneakers</h1>
+            <span className="subtitle">Shop Here </span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="heading">T-Shirts </h1>
+            <span className="subtitle">Shop Here </span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="heading">T-Shirts </h1>
+            <span className="subtitle">Shop Here </span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="heading">T-Shirts </h1>
+            <span className="subtitle">Shop Here </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
