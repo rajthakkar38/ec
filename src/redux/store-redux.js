5a487b3d36d0dca4a3165719df7f4fc7a4315728
Raj <raj.21810934@viit.ac.in>
2021-06-09 16:57:22 +0530

    random

--- src/redux/store-redux.js
+++ src/redux/store-redux.js
@@ -6,5 +6,9 @@ import rootReducer from "./root-redux";
 
 const middlewares = [logger];
 
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
